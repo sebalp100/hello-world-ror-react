fa195fcc99b1dfbbc89d743a329aa29b84e6af7d
sebalp100 <sebalp100@gmail.com>
2023-04-17 19:36:15 -0300

    Separate store and slice

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,7 +4,7 @@ import "./controllers"
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { store } from './store';
+import store from './store';
 import Greeting from './greeting';
 
 ReactDOM.render(
--- app/javascript/greeting.js
+++ app/javascript/greeting.js
@@ -1,9 +1,10 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchGreeting } from './store';
+import { fetchGreeting } from './greetingSlice';
 
 function Greeting() {
   const dispatch = useDispatch();
+
   const greeting = useSelector(state => state.greeting.greeting);
   const loading = useSelector(state => state.greeting.loading);
   const error = useSelector(state => state.greeting.error);
